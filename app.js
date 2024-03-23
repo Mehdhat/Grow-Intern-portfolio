4a54dd6c9faf00bd181285b7319d0bbbdb5f5748
Mehdhat <mehdhathafeez@gmail.com>
2024-03-23 10:13:01 +0500

    ---

--- app.js
+++
@@ -1,8 +0,0 @@
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar');
-sidebar.computedStyleMap.display='flex';
-}
-function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.computedStyleMap.display='none';
-}
